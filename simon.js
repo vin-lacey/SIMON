5816a51818c91fc63b6ec005809e12be8d7155d5
Vin Lacey <vin.lacey@aol.com>
2021-06-17 23:27:31 +0100

    add random_color function and test

--- simon.js
+++ simon.js
@@ -1,6 +1,19 @@
-// SIMON's JavaScript version 0.1
+// SIMON's JavaScript version 0.1 alpha by Vin Lacey
 
+// const COLORS = ["red", "green", "blue", "yellow"]
 const COLORS = ["red", "green", "blue", "yellow"]
-
 // we need to create an array of choices
 // which grows depending on round and level
+let current_game_array = []
+
+const random_color = () => {
+
+  return COLORS[ Math.floor( Math.random() * 4 ) ]
+}
+
+// test 10 colours
+for (let i = 0; i < 10; i++) {
+  current_game_array.push(random_color())
+}
+
+console.log(current_game_array)
